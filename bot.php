1c116a976ecc87f325c0cdccfd8dc573b30de645
hj2 <jong@windowslive.com>
2017-01-04 22:33:09 +0700

    Update

--- bot.php
+++ bot.php
@@ -1,2 +1,15 @@
 <?php
-echo "I am a bot";
+$access_token = 'yJ0gNoR1OR5PlMtLhfJG7rPnlgp55CNEocKN0MpRKOCq7QEigwE+wbCBN9vl2tQ5wC+SxBtjreOi6u7bjT0vqsO4cx7PeGm4oVZFqN6sL7FUJacYjFWDxfSWDs50timRTLl7dnncvewjvh8JnbcIFgdB04t89/1O/w1cDnyilFU=';
+
+$url = 'https://api.line.me/v1/oauth/verify';
+
+$headers = array('Authorization: Bearer ' . $access_token);
+
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+$result = curl_exec($ch);
+curl_close($ch);
+
+echo $result;
