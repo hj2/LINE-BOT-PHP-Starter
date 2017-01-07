d71bcd1f1db1a896cb3ca385ec36e962a508d876
Jong <jong@windowslive.com>
2017-01-07 19:50:48 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -24,10 +24,15 @@ if (!is_null($events['events'])) {
 
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
-			$data = [
+			/*$data = [
 				'replyToken' => $replyToken,
 				'messages' => [$messages],
-			];
+			];*/
+			$data = [
+				 "type": "sticker",
+				  "packageId": "1",
+				  "stickerId": "1",
+				];
 			$post = json_encode($data);
 			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
 
